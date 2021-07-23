1fd2390c904789d36bd2a8ddd941184bfc7c7eaa
Chris Kaiser <chris.kaiser@wolterskluwer.com>
2021-07-23 22:40:18 +0200

    Initial commit of empty project

---
+++ src/main/java/de/bahmut/kindleproxy/KindleProxyApplication.java
@@ -0,0 +1,13 @@
+package de.bahmut.kindleproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindleProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KindleProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/bahmut/kindleproxy/KindleProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bahmut.kindleproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindleProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
